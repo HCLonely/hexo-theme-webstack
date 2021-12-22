3bc9e40879cf7f5d452bfde209f70c7f3f0cd890
HCLonely <h1606051253@gmail.com>
2021-12-22 10:23:51 +0800

    Fixed #16

--- move_config.js
+++ move_config.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 
 if (fs.existsSync('../hexo/package.json')){
   const version = JSON.parse(fs.readFileSync('../hexo/package.json')).version
-  if (version.split('.')[0].imcludes('5')) {
+  if (version.split('.')[0].includes('5')) {
     const oldConfigPath = '../../source/_data/webstack.yml'
     const configPath = '../../_config.webstack.yml'
     if (!fs.existsSync(configPath) && !fs.existsSync(oldConfigPath)) {
