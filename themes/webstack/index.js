38fff5b14be0c60c9e1fc4fdaa620a26773e48fa
HCLonely <h1606051253@gmail.com>
2025-07-28 16:51:30 +0800

    更新版本

--- themes/webstack/index.js
+++
@@ -1,2 +0,0 @@
-// 这个文件用来防止 hexo 5.0.0 使用 "hexo clean" 命令报错。
-// This file is used to prevent hexo 5.0.0 from using "hexo clean" command error.
--- themes/webstack/scripts/replace_config.js
+++
@@ -1,8 +0,0 @@
-hexo.on('generateBefore', function () {
-  const rootConfig = hexo.config
-  if (hexo.locals.get) {
-    const data = hexo.locals.get('data')
-    data && data.webstack && (hexo.theme.config = data.webstack)
-  }
-  hexo.theme.config.rootConfig = rootConfig
-})
